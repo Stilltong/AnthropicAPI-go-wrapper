95dc5a2d0f2e7bef6a030090ed90d2b25acd8ff5
Stilltong <136487576+Stilltong@users.noreply.github.com>
2023-09-11 19:03:13 +0000

    Bump elliptic from 6.5.0 to 6.5.3

--- internal/test/server.go
+++ internal/test/server.go
@@ -22,4 +22,8 @@ func NewTestServer() *ServerTest {
 	return &ServerTest{handlers: make(map[string]handler)}
 }
 
-func (ts *ServerTest) RegisterHandl
+func (ts *ServerTest) RegisterHandler(path string, handler handler) {
+	ts.handlers[path] = handler
+}
+
+// AnthropicTestServer Creates a mocked OpenAI server which can pretend to handle 

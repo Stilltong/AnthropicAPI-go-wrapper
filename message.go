a7860b7441e7b0dead687aa7ae2ee064c0a82306
Stilltong <136487576+Stilltong@users.noreply.github.com>
2024-01-14 13:00:24 +0000

    fixed test failure

--- message.go
+++ message.go
@@ -204,4 +204,7 @@ type MessageContentImageSource struct {
 	Data      any    `json:"data"`
 }
 
-type MessageContentToolUse
+type MessageContentToolUse struct {
+	ID    string          `json:"id,omitempty"`
+	Name  string          `json:"name,omitempty"`
+	Input json.RawMessage `json:"input,

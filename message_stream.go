162f3fe7f58730c3876201e084be08c2940dedab
Stilltong <136487576+Stilltong@users.noreply.github.com>
2024-01-29 19:35:05 +0000

    test change

--- message_stream.go
+++ message_stream.go
@@ -117,3 +117,123 @@ func (c *Client) CreateMessagesStream(ctx context.Context, request MessagesStrea
 	for {
 		rawLine, readErr := reader.ReadBytes('\n')
 		if readErr != nil {
+			if errors.Is(readErr, io.EOF) {
+				break
+			}
+			return response, readErr
+		}
+
+		noSpaceLine := bytes.TrimSpace(rawLine)
+		if len(noSpaceLine) == 0 {
+			continue
+		}
+		if bytes.HasPrefix(noSpaceLine, eventPrefix) {
+			event = bytes.TrimSpace(bytes.TrimPrefix(noSpaceLine, eventPrefix))
+			continue
+		}
+		if bytes.HasPrefix(noSpaceLine, dataPrefix) {
+			var (
+				data      = bytes.TrimPrefix(noSpaceLine, dataPrefix)
+				eventType = MessagesEvent(event)
+			)
+			switch eventType {
+			case MessagesEventError:
+				var eventData ErrorResponse
+				if err := json.Unmarshal(data, &eventData); err != nil {
+					return response, err
+				}
+				if request.OnError != nil {
+					request.OnError(eventData)
+				}
+				return response, eventData.Error
+			case MessagesEventPing:
+				var d MessagesEventPingData
+				if err := json.Unmarshal(data, &d); err != nil {
+					return response, err
+				}
+				if request.OnPing != nil {
+					request.OnPing(d)
+				}
+				continue
+			case MessagesEventMessageStart:
+				var d MessagesEventMessageStartData
+				if err := json.Unmarshal(data, &d); err != nil {
+					return response, err
+				}
+				if request.OnMessageStart != nil {
+					request.OnMessageStart(d)
+				}
+				response = d.Message
+				continue
+			case MessagesEventContentBlockStart:
+				var d MessagesEventContentBlockStartData
+				if err := json.Unmarshal(data, &d); err != nil {
+					return response, err
+				}
+				if request.OnContentBlockStart != nil {
+					request.OnContentBlockStart(d)
+				}
+				response.Content = slices.Insert(response.Content, d.Index, d.ContentBlock)
+				continue
+			case MessagesEventContentBlockDelta:
+				var d MessagesEventContentBlockDeltaData
+				if err := json.Unmarshal(data, &d); err != nil {
+					return response, err
+				}
+				if request.OnContentBlockDelta != nil {
+					request.OnContentBlockDelta(d)
+				}
+				if len(response.Content)-1 < d.Index {
+					response.Content = slices.Insert(response.Content, d.Index, d.Delta)
+				} else {
+					response.Content[d.Index].MergeContentDelta(d.Delta)
+				}
+				continue
+			case MessagesEventContentBlockStop:
+				var d MessagesEventContentBlockStopData
+				if err := json.Unmarshal(data, &d); err != nil {
+					return response, err
+				}
+				var stopContent MessageContent
+				if len(response.Content) > d.Index {
+					stopContent = response.Content[d.Index]
+					if stopContent.Type == MessagesContentTypeToolUse {
+						stopContent.Input = json.RawMessage(*stopContent.PartialJson)
+						stopContent.PartialJson = nil
+						response.Content[d.Index] = stopContent
+					}
+				}
+				if request.OnContentBlockStop != nil {
+					request.OnContentBlockStop(d, stopContent)
+				}
+				continue
+			case MessagesEventMessageDelta:
+				var d MessagesEventMessageDeltaData
+				if err := json.Unmarshal(data, &d); err != nil {
+					return response, err
+				}
+				if request.OnMessageDelta != nil {
+					request.OnMessageDelta(d)
+				}
+				response.StopReason = d.Delta.StopReason
+				response.StopSequence = d.Delta.StopSequence
+				response.Usage.OutputTokens = d.Usage.OutputTokens
+				continue
+			case MessagesEventMessageStop:
+				var d MessagesEventMessageStopData
+				if err := json.Unmarshal(data, &d); err != nil {
+					return response, err
+				}
+				if request.OnMessageStop != nil {
+					request.OnMessageStop(d)
+				}
+				continue
+			}
+		}
+		emptyMessageCount++
+		if emptyMessageCount > c.config.EmptyMessagesLimit {
+			return response, ErrTooManyEmptyStreamMessages
+		}
+	}
+	return
+}
---
+++ message_stream_test.go
@@ -0,0 +1,14 @@
+
+package anthropic_test
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/liushuangls/go-anthropic/v2"
+	"github.com/liushuangls/go-anthropic/v2/internal/test"
+	"github.com/liushuangls/go-anthropic/v2/jsonschema"
+)
